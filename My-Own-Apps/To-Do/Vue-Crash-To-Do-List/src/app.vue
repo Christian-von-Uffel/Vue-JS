dda5ea4cb2559f70ecfad3431237f0595754306b
Christian von Uffel <christian@cvonu.com>
2020-08-17 14:24:42 -0400

    Create To-Do app

---
+++ My-Own-Apps/To-Do/Vue-Crash-To-Do-List/src/app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+  import todos from '.components/todos';
+  export default {
+    name: 'app',
+    components: {
+
+    },
+    data(): {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: "Todo one",
+            completed: false
+          },
+          {
+            id: 2,
+            title: "Todo two",
+            completed: true
+          },
+          {
+            id: 3,
+            title: "Todo three",
+            completed: false
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  /* mmm, wild card, I like it */
+  * {
+
+  }
+</style>
---
+++ My-Own-Apps/To-Do/Vue-Crash-To-Do-List/src/components/todos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- I think the v-bind directive helps Vue know what order to execute the loop in -->
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{todo.title}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: "todos",
+    props: ["todos"],
+    methods: {
+      markComplete() {
+        // this is pretty cool that you can just flip a boolean attribute using the not operator. I honestly just didn't know you could do this
+        this.todo.completed = !this.todo.completed;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
