f6d5aa08b38504b9df9a53313c6456ce6d03e0e5
Christian von Uffel <christian@cvonu.com>
2020-11-19 16:57:41 -0500

    replace simple scripts with methods

--- Vue-JS-The-Complete-Guide/basics-03-events-starting-code/app.js
+++ Vue-JS-The-Complete-Guide/basics-03-events-starting-code/app.js
@@ -2,5 +2,13 @@ const app = new Vue({
   el: "#events",
   data:{
       counter: 0,
+  },
+  methods:{
+    add(){
+      this.counter = this.counter + 1;
+    },
+    subtract(){
+      this.counter = this.counter - 1;
+    },
   }
 });
